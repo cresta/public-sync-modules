c9f5488f13009223fa407ffa9629931091b47bb7
Jack Lindamood <jack@cresta.ai>
2023-07-20 16:16:15 -0700

    nit: checkpoint commit

--- opensourcegocli/tools.go
+++ opensourcegocli/tools.go
@@ -8,6 +8,7 @@ import (
 	_ "github.com/cresta/public-sync-modules/autoapprove"
 	_ "github.com/cresta/public-sync-modules/buildgolib"
 	_ "github.com/cresta/public-sync-modules/golangcilint"
+	_ "github.com/cresta/public-sync-modules/goreleasercli"
 	_ "github.com/cresta/public-sync-modules/setlicense"
 	_ "github.com/cresta/public-sync-modules/synceractions"
 )
