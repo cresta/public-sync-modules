a9e47339d69fc428bd62ec590cf9b8318db00e95
Jack Lindamood <jack@cresta.ai>
2023-07-20 16:08:55 -0700

    feat: support go releaser

---
+++ goreleasercli/module.go
@@ -0,0 +1,30 @@
+package goreleasercli
+
+import (
+	_ "embed"
+
+	"github.com/cresta/syncer/sharedapi/drift/templatefiles"
+	"github.com/cresta/syncer/sharedapi/syncer"
+)
+
+func init() {
+	syncer.FxRegister(Module)
+}
+
+//go:embed .goreleaser.yaml.template
+var templateStrGoReleaser string
+
+//go:embed goreleaser.yaml.template
+var templateStrActionReleaser string
+
+var Module = templatefiles.NewModule(templatefiles.NewModuleConfig[Config]{
+	Name: "goreleasercli",
+	Files: map[string]string{
+		".goreleaser.yaml":                  templateStrGoReleaser,
+		".github/workflows/goreleaser.yaml": templateStrActionReleaser,
+	},
+	Priority: syncer.PriorityNormal,
+	Decoder:  templatefiles.DefaultDecoder[Config](),
+})
+
+type Config struct{}
---
+++ opensourcegocli/module.go
@@ -0,0 +1,16 @@
+package opensourcegocli
+
+import (
+	_ "embed"
+
+	"github.com/cresta/syncer/sharedapi/syncer"
+)
+
+func init() {
+	syncer.FxRegister(Module)
+}
+
+//go:embed config.yaml
+var configYaml []byte
+
+var Module = syncer.NewChildModule("github.com/cresta/public-sync-modules/opensourcegocli", configYaml)
