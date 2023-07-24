9fd4de6fa7e77a92ca517efc4b197111aa6b18c7
Jack Lindamood <jack@cresta.ai>
2023-07-24 13:13:05 -0700

    checkpoint commit

--- opensourcegocli/tools.go
+++ opensourcegocli/tools.go
@@ -5,7 +5,6 @@ package opensourcegocli
 
 // TODO: Auto generate this from the config.yaml file
 import (
-	_ "github.com/getsyncer/public-sync-modules/autoapprove"
 	_ "github.com/getsyncer/public-sync-modules/buildgolib"
 	_ "github.com/getsyncer/public-sync-modules/gitignore"
 	_ "github.com/getsyncer/public-sync-modules/golangcilint"
