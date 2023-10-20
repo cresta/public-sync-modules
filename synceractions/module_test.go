70a3328c4d34d9d6d828e287e83f7fc0e9ce8bae
Jack Lindamood <jack@cresta.ai>
2023-10-20 10:39:23 -0700

    fix: fix generated files

--- synceractions/module_test.go
+++ synceractions/module_test.go
@@ -19,4 +19,19 @@ syncs:
 		drifttest.FileContains(t, ".github/workflows/watchsynccomment.yaml", "github.event.comment.body")
 		drifttest.OnlyGitChanges(t, ".github/workflows/watchsynccomment.yaml", ".github/workflows/checksync.yaml")
 	}))
+	config2 := `
+version: 1
+config:
+  repush_on_actor: bob
+logic:
+  - source: github.com/getsyncer/syncer-core/drift/syncers/synceractions
+syncs:
+  - logic: synceractions
+`
+	t.Run("make-new-file", drifttest.WithRun(config2, drifttest.ReasonableSampleFilesystem(), func(t *testing.T, items *drifttest.Items) {
+		items.TestRun.MustExitCode(t, 0)
+		drifttest.FileContains(t, ".github/workflows/watchsynccomment.yaml", "github.event.comment.body")
+		drifttest.OnlyGitChanges(t, ".github/workflows/watchsynccomment.yaml", ".github/workflows/checksync.yaml")
+		drifttest.FileIsYAML(t, ".github/workflows/watchsynccomment.yaml")
+	}))
 }
